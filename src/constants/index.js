a449b0461396d570749a169f4a932c6f0ff999c4
Lucas <contatolucasm9@gmail.com>
2023-03-21 12:30:56 -0300

    done

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,21 +25,21 @@ export const features = [
     icon: star,
     title: "Rewards",
     content:
-      "The best credit cards offer some tantalizing combinations of promotions and prizes",
+      "Os melhores cartões de crédito oferecem algumas combinações tentadoras de promoções e prêmios.",
   },
   {
     id: "feature-2",
     icon: shield,
     title: "100% Secured",
     content:
-      "We take proactive steps make sure your information and transactions are secure.",
+      "Tomamos medidas proativas para garantir que suas informações e transações estejam seguras.",
   },
   {
     id: "feature-3",
     icon: send,
     title: "Balance Transfer",
     content:
-      "A balance transfer credit card can save you a lot of money in interest charges.",
+      "Um cartão de crédito de transferência de saldo pode economizar muito dinheiro em juros.",
   },
 ];
 
